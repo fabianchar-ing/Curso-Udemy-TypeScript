7b0a7f42ce287fd182763f343d28c8b5c3cc8cdb
fabianchar-ing <fabiancharris@outlook.com>
2022-03-06 22:35:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-byyerm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-byyerm)
---
+++ index.ts
@@ -0,0 +1,9 @@
+let palabra: string = 'Hola';
+function sumar(a, b) {
+  return a + b;
+}
+
+console.log(sumar('Fabian', 'Charris'));
+
+// IMPORTANTE:
+// Ver la consola de JavaScript
